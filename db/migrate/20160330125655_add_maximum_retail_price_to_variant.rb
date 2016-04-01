ef39ccac4fb52c6b5754d81017e258c9cf1a8409
sawan gupta <sawan.gupta92@gmail.com>
2016-04-01 19:20:05 +0530

    fix migration

--- db/migrate/20160330125655_add_maximum_retail_price_to_variant.rb
+++ db/migrate/20160330125655_add_maximum_retail_price_to_variant.rb
@@ -1,13 +1,13 @@
 class AddMaximumRetailPriceToVariant < ActiveRecord::Migration
   def up
-    add_column :spree_variants, :maximum_retail_price, :decimal, scale: 8, precision: 2, default: 0
+    add_column :spree_variants, :maximum_retail_price, :decimal, precision: 8, scale: 2, default: 0
     Spree::Variant.all.each do |variant|
       variant.update_column(:maximum_retail_price, variant.price)
     end
   end
 
   def down
-    remove_column :spree_variants, :maximum_retail_price, :decimal, scale: 8, precision: 2, default: 0
+    remove_column :spree_variants, :maximum_retail_price, :decimal, precision: 8, scale: 2, default: 0
   end
 
 end
