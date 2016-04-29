e3ec816ec14c85de9a6937f0f1f5ad3943be14cd
sawan gupta <sawan.gupta92@gmail.com>
2016-04-29 17:20:40 +0530

    move cache key to spree_themes

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -5,5 +5,11 @@ module Spree
       max_updated_at = (@products.try(:maximum, :updated_at) || Date.today).to_s(:number)
       "#{I18n.locale}/#{current_currency}/spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}-#{Spree::Config[:theme_name]}-#{Spree::Taxon.pluck(:is_featured).to_sentence}"
     end
+
+    def cache_key_for_taxons
+      max_updated_at = @taxons.maximum(:updated_at).to_i
+      parts = [@taxon.try(:id), max_updated_at].compact.join("-")
+      "#{I18n.locale}/taxons/#{parts}/#{Spree::Config[:theme_name]}"
+    end
   end
 end
