3facc084071a15d75697c9da3af08e49c8842b6c
PareshGupta <paresh@vinsol.com>
2017-07-10 11:31:22 +0530

    expire fragment caching

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -3,7 +3,12 @@ Spree::StoreController::THEME_VIEW_LOAD_PATH = File.join(Spree::Theme::CURRENT_T
 module Spree
   StoreController.class_eval do
 
+    fragment_cache_key Spree::FragmentCacheController::CACHE_KEY
+
     prepend_view_path Spree::ThemesTemplate::Resolver.new(Spree::StoreController::THEME_VIEW_LOAD_PATH)
+
+    # FIX_ME_PG:- Currently expiring all frament cache. Need to clear only when caching is present in page.
+    before_action ->() { Spree::FragmentCacheController.new.clear_cache }
 
   end
 end
