9cd7a3707fb7f9356463efa090426488a773d109
PareshGupta <paresh@vinsol.com>
2017-07-26 21:21:03 +0530

    Updated gem version

--- lib/vinsol_spree_themes/version.rb
+++ lib/vinsol_spree_themes/version.rb
@@ -11,8 +11,8 @@ module VinsolSpreeThemes
     MAJOR = 0
     MINOR = 0
     TINY  = 1
-    PRE   = ''.freeze
+    
 
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
 end
